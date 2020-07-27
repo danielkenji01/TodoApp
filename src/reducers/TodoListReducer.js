d341bd374a544eb246a4b453c21681bfdca34169
danielkenji01 <daniel_kenji_1@hotmail.com>
2020-07-27 13:03:23 -0300

    Criação da action de clique no TODO

--- src/reducers/TodoListReducer.js
+++ src/reducers/TodoListReducer.js
@@ -1,16 +1,30 @@
-import { ADD_TODO } from '../actions';
+import { ADD_TODO, TOGGLE_TODO } from '../actions';
 
 let nextId = 1;
 
 const todoListReducer = (state = [], action) => {
     switch (action.type) {
         case ADD_TODO:
-            console.log('adicionou todo!', action.text);
+            const { text } = action;
+            console.log('adicionou todo!', text);
             const newTodo = {
                 id: nextId++,
-                text: action.text
+                text,
+                done: false
             }
             return [...state, newTodo];
+        case TOGGLE_TODO: 
+            const { todoId } = action;
+            return state.map(todo => {
+                if (todo.id === todoId) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    };
+                }
+
+                return todo;
+            });
         default:
             return state;
     }
